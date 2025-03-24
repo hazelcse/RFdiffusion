364a9ce61f3ebc5dacebf985a50ef6aa8d97def6
hazelcse <hazelcse02@gmail.com>
2025-03-24 19:33:51 +0800

    wow MORE issues

--- potentials/potentials.py
+++ potentials/potentials.py
@@ -52,7 +52,7 @@ class docking_score(Potential):
 
     def __init__(self, weight=1, ligand_features=None):
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        self.weight = weight.to(device)
+        self.weight = weight
         self.ligand_features = ligand_features.to(device)
         print(ligand_features)
         self.surrogate_model = torch.load('surrogate_nn.pth').to(device).eval()
